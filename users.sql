b298f713e76c9cb2da52de548a7713eedcf8193d
KhateebAhmad <x16112989@student.ncirl.ie>
2017-04-06 10:55:59 +0100

    updating scripts

--- users.sql
+++ users.sql
@@ -1,26 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.3.9
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Dec 26, 2016 at 07:46 PM
--- Server version: 5.5.49-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.20
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+create database doc_app;
+use doc_app;
 
---
--- Database: `codeigniter`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
@@ -60,3 +44,6 @@ ALTER TABLE `users`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+insert into users (name, password) values ('admin', MD5('admin'));
